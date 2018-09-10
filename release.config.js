b33da825ee3ea993482daf294ef8d840faf09280
Eric Urbina <urbina.eric@gmail.com>
2018-09-10 02:45:49 -0600

    Reverting some changes on release.config.js

--- release.config.js
+++ release.config.js
@@ -19,11 +19,11 @@ module.exports = {
     publish: [
         // https://github.com/semantic-release/changelog
         // Set of semantic-release plugins for creating or updating a changelog file.
-         '@semantic-release/changelog',
+        // '@semantic-release/changelog',
 
         // https://github.com/semantic-release/git
         // Git plugin is need so the changelog file will be committed to the Git repository and available on subsequent builds in order to be updated.
-         '@semantic-release/git',
+        // '@semantic-release/git',
 
         // https://github.com/semantic-release/git
         // Exec plugin uses to call dotnet nuget push to push the packages from 
